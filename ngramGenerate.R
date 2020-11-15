5f6ea2c9517556b65ac97aa0445ba2a56fd20979
Batata <Zhangzixin.dalian@gmail.com>
2020-11-15 16:28:04 +0100

    fixing non english letter problem

--- ngramGenerate.R
+++ ngramGenerate.R
@@ -5,7 +5,11 @@
 library(tm)
 library(Stem)
 library(RWeka)
-library(quanteda)
+# library(quanteda)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(dplyr)
 
 set.seed(125)
 ###----- download and read data -----###
@@ -26,51 +30,75 @@ con <- file("final/en_US/en_US.news.txt", "r")
 enNews <- readLines(con, -1)
 close(con)
 
-preprocessData <- function(data, sampleSize)
+###----- Testing and Training model -----###
+
+phraseList <- list(Twitter = enTwitter[1:floor(0.8*length(enTwitter))],Blogs = enBlogs,News = enNews)
+
+getNgram <- function(corpus, ngram)
+{
+    BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
+    TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+    QuadgramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+    
+    tdm.ngramText <- switch (as.String(ngram),
+                             '2' = TermDocumentMatrix(tCorpus, control = list(tokenize = BigramTokenizer)),
+                             '3' = TermDocumentMatrix(tCorpus, control = list(tokenize = TrigramTokenizer)),
+                             '4' = TermDocumentMatrix(tCorpus, control = list(tokenize = QuadgramTokenizer)),
+                             stop("Only 2,3 and 4 are used for ngram model")
+                             )
+    
+    ngramMatrx <- as.matrix(tdm.ngramText)
+    returnList <- list(tdm.ngramText, ngramMatrx)
+}
+
+
+preprocessData <- function(data, sampleSize, N_Gram)
 {
     ## sample the dataset of each category
     dataSample = data[sample(c(1:length(data)), size = sampleSize, replace = F)]
     ## pre-process: remove excess blanks, lower all letters, remove non-alphabet text
     dataPostPro = stripWhitespace(
            tolower(
-           gsub('[[:digit:]]+', '',
-           gsub('[[:punct:]]','', unlist(strsplit(dataSample, '\\.+|\\!+|\\?+|\\,+'))))))
+           # gsub('[[:digit:]]+', '',
+           gsub('[^\x01-\x7F]+', '',
+           gsub('[^[:alpha:][:blank:]]','', unlist(strsplit(dataSample, '\\.+|\\!+|\\?+|\\,+'))))))
+
+    ## transform to TDM format and Matrix format
+    cat(dataPostPro, file = "final/en_US/sample.txt",sep = "\n")
+    fileDirSource <- DirSource("final/en_US/", pattern = "sample.txt")
+    textCorpus <- VCorpus(fileDirSource, readerControl = list(language = "English"))
+    textTdmMatrix <- getNgram(textCorpus, N_Gram)
+    returnList <- list(textPostPro = dataPostPro, 
+                       textTDM = textTdmMatrix[[1]], 
+                       ngramMatrx = textTdmMatrix[[2]])
+    ## 
 }
 
-phraseList <- list(Twitter = enTwitter,Blogs = enBlogs,News = enNews)
 sampleSize <-  200
-sampleResult <- lapply(phraseList, function(data) preprocessData(data,sampleSize))
-
-cat(sampleResult[[1]],file = "final/en_US/twitter_sample.txt",sep = "\n")
-cat(sampleResult[[2]],file = "final/en_US/blogs_sample.txt",sep = "\n")
-cat(sampleResult[[3]],file = "final/en_US/news_sample.txt",sep = "\n")
-
-fileTwitter <- DirSource("final/en_US/", pattern = "twitter_sample.txt")
-fileBlogs <- DirSource("final/en_US/", pattern = "blogs_sample.txt")
-fileNews <- DirSource("final/en_US/", pattern = "news_sample.txt")
-tCorpus <- VCorpus(fileTwitter, readerControl = list(language = "English"))
-bCorpus <- VCorpus(fileBlogs, readerControl = list(language = "English"))
-nCorpus <- VCorpus(fileNews, readerControl = list(language = "English"))
-
-testng <- NGramTokenizer(tCorpus, Weka_control(min = 2, max = 2))
-
-tdm.bigram <- TermDocumentMatrix(tCorpus, control = list(tokenize = testng))
-
-
-getNgram <- function(corpus, ngram)
+N_Gram <- c(2:4)
+for (n in N_Gram)
 {
-    BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
-    TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
-    QuadgramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 4, max = 4))
-    
-    tdm.ngramText <- switch (ngram,
-                             '2' = TermDocumentMatrix(tCorpus, control = list(tokenize = BigramTokenizer)),
-                             '3' = TermDocumentMatrix(tCorpus, control = list(tokenize = TrigramTokenizer)),
-                             '4' = TermDocumentMatrix(tCorpus, control = list(tokenize = QuadgramTokenizer)),
-                             stop("Only 2,3 and 4 are used for ngram model"))
-    
-    ngramMatrx <- as.matrix(tdm.ngramText)
+    varName <- paste("textPostProcess", n, 'Gram', sep = "")
+    assign(varName, lapply(phraseList, function(data) preprocessData(data, sampleSize, n)))
+
 }
+sampleResult <- lapply(phraseList, function(data) preprocessData(data,sampleSize, N_Gram))
+
+# cat(sampleResult[[1]],file = "final/en_US/twitter_sample.txt",sep = "\n")
+# cat(sampleResult[[2]],file = "final/en_US/blogs_sample.txt",sep = "\n")
+# cat(sampleResult[[3]],file = "final/en_US/news_sample.txt",sep = "\n")
+# 
+# fileTwitter <- DirSource("final/en_US/", pattern = "twitter_sample.txt")
+# fileBlogs <- DirSource("final/en_US/", pattern = "blogs_sample.txt")
+# fileNews <- DirSource("final/en_US/", pattern = "news_sample.txt")
+# tCorpus <- VCorpus(fileTwitter, readerControl = list(language = "English"))
+# bCorpus <- VCorpus(fileBlogs, readerControl = list(language = "English"))
+# nCorpus <- VCorpus(fileNews, readerControl = list(language = "English"))
+# 
+
+
+
+
 
 ##### findAssocs(crudeTDM, "oil", 0.85)
 
