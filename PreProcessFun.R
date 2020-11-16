c38f82bdc92c5b92618250f958eff571a0cf21a4
Batata <Zhangzixin.dalian@gmail.com>
2020-11-16 20:17:38 +0100

    seperate Tokens

--- PreProcessFun.R
+++ PreProcessFun.R
@@ -1,58 +1,17 @@
-#### test code of the tm package
-## dataset source
-# url = "http://kdd.ics.uci.edu/databases/reuters21578/reuters21578.html"
-
-library(tm)
-library(Stem)
-library(RWeka)
-# library(quanteda)
-library(tidyr)
-library(stringr)
-library(ggplot2)
-library(dplyr)
-
-set.seed(125)
-###----- download and read data -----###
-setwd('D:/Coursera/Datascience/Capstone')
-url = 'https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip'
-destfile = "Coursera-SwiftKey.zip"    
-if (!file.exists(destfile)) {
-    download.file(fileURL, destfile, method="auto")
-    unzip("Coursera-SwiftKey.zip", overwrite = FALSE)}
-
-con <- file("final/en_US/en_US.twitter.txt", "r")
-enTwitter <- readLines(con, -1)
-close(con)
-con <- file("final/en_US/en_US.blogs.txt", "r")
-enBlogs <- readLines(con, -1)
-close(con)
-con <- file("final/en_US/en_US.news.txt", "r")
-enNews <- readLines(con, -1)
-close(con)
-
-###----- Testing and Training model -----###
-
-textAll <- c(enTwitter, enBlogs, enNews)
-samplePercent = 0.005
-textSample <- textAll[sample(c(1:length(textAll)), size = length(textAll)*samplePercent, replace = F)]
-indexTrain <- sample(c(1:length(textSample)), size = length(textSample)*0.8, replace = F)
-textTrain <- textSample[indexTrain]
-textTest <- textSample[-indexTrain]
-
-getNgram <- function(corpus, ngram)
+getNgram <- function(corpus, N_Gram)
 {
     BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
     TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
     QuadgramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 4, max = 4))
     
-    ngramTextTDM <- switch (as.String(ngram),
+    ngramTextTDM <- switch (as.String(N_Gram),
                             '2' = TermDocumentMatrix(corpus, control = list(tokenize = BigramTokenizer)),
                             '3' = TermDocumentMatrix(corpus, control = list(tokenize = TrigramTokenizer)),
                             '4' = TermDocumentMatrix(corpus, control = list(tokenize = QuadgramTokenizer)),
                             stop("Only 2,3 and 4 are used for ngram model")
     )
     
-    ngramMatrx <- as.matrix(counts = ngramTextTDM)
+    ngramMatrx <- as.matrix(ngramTextTDM)
     returnList <- list(ngramTextTDM, ngramMatrx)
 }
 
@@ -68,7 +27,7 @@ getNgramTokenMatrix <- function(DF, N_Gram)
                                 '4' = c('g1', 'g2', 'g3', 'g4'))
     
     tokens <- data.frame(tokens) %>%
-        mutate(counts = matrix(DF)) %>%
+        cbind(.,counts = DF) %>%
         group_by(g1)
 }
 
@@ -92,14 +51,3 @@ preprocessData <- function(data, N_Gram)
                        textTDM = textTdmMatrix[[1]], 
                        ngramMatrx = tokenMatrix)
 }
-
-N_Gram <- c(2:4)
-for (n in N_Gram)
-{
-    varName <- paste("text", n, 'Gram', sep = "")
-    assign(varName, preprocessData(textTrain, n))
-
-}
-
-##### findAssocs(crudeTDM, "oil", 0.85)
-
